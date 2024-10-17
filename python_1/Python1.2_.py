97120561a3bdb6138abc3c0a81996a958688e781
cloelovesroblox <cloekalea@gmail.com>
2024-10-17 16:24:37 -0400

    Pushing my python and logic algorithm

---
+++ python_1/Python1.2_.py
@@ -0,0 +1,8 @@
+username = "Watermelon"
+nickname = "kooiaid"
+age = 16
+bankAccountValue = 25.50
+lotteryamount = 600000000
+
+print("Your account balance is" bankAccountValue)
+print("watermelon" , username)
