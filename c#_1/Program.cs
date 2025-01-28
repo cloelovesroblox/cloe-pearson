3341432bdab88c1cb10be68897573390a0a65056
cloelovesroblox <cloekalea@gmail.com>
2025-01-28 17:05:16 -0500

    Today I have finished my gaming and adding an animation and also I started to do my C# aswell

--- c#_1/Program.cs
+++ c#_1/Program.cs
@@ -1,2 +1,39 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Runtime.ExceptionServices;
+using System.Runtime.InteropServices;
+
+Console.WriteLine("Welcome, Saniro charaters ");
+Console.WriteLine("Welcome, Saniro Dressup");
+
+
+
+
+bool HelloKittyblue = false;
+
+bool HelloKittyred = true;
+
+bool HelloKittygreen = false;
+
+if (HelloKittyred)
+{
+    Console.WriteLine("Hello Kitty is red and no other color"); 
+}
+else 
+{
+    Console.WriteLine("Hello kitty is not green she is red");
+}
+
+
+
+
+
+int temperature = 30;
+
+if (temperature > 15)
+{
+    Console.WriteLine("We are going to wear pink");
+}
+else
+{
+    Console.WriteLine("We are going to wear blue");
+}
