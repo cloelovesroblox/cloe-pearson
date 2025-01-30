df4d89b9ee8add3dee53bf522220ab300c18bf25
cloelovesroblox <cloekalea@gmail.com>
2025-01-30 17:07:38 -0500

    Today my programs have been fixed and finishing the rest of my work

--- c#_1/Program.cs
+++ c#_1/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.CompilerServices;
 using System.Runtime.ExceptionServices;
 using System.Runtime.InteropServices;
 
@@ -13,6 +14,10 @@ bool HelloKittyblue = false;
 bool HelloKittyred = true;
 
 bool HelloKittygreen = false;
+int Maxharacter= 5;
+string CharacterSays = "hello, thank you for picking me & let's get this party started";
+
+int temperature = 30;
 
 if (HelloKittyred)
 {
@@ -27,7 +32,7 @@ else
 
 
 
-int temperature = 30;
+
 
 if (temperature > 15)
 {
@@ -37,3 +42,49 @@ else
 {
     Console.WriteLine("We are going to wear blue");
 }
+
+
+
+
+
+
+
+
+
+int[] numbers = { 2, 4, 6, 8, 10, 12 };
+
+Console.WriteLine(numbers[1]);
+
+List<string> family = new List<string>();
+
+family.Add("London");
+family.Add("Tre");
+family.Add("Kimora");
+
+
+for (int i = 1; i < family.Count; i++){
+    Console.WriteLine(family[i]);
+
+}
+
+
+
+
+
+
+int charatersMade = 0;
+
+while (charatersMade < 7)
+
+{
+    Console.WriteLine("charatersMade");
+    charatersMade++;
+}
+
+for (int Londons = 0; Londons < 15; Londons++);
+{
+    Console.WriteLine("Clothes Loaded ");
+}
+
+
+
---
+++ c#_1/sun.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sun : MonoBehaviour
+{
+   
+   
+
+    // Update is called once per frame
+     public int myNum = 88;
+     private int number2;
+        
+    void Start()
+    {
+       
+        Console.WriteLine(myNum);
+
+        
+        Console.WriteLine(myNum);
+
+        Debug.Log("Welcome to my game");
+        Debug.Log("myfriend");
+        Debug.Log("100");
+        int result = Add(5,5);
+     
+    }
+    int Add(int a, int b);
+    {
+        return a + b;
+    }
+
+
+
+
+}
+
